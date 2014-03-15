6627fe0f224e42d97777f19f13b99f0395ea35ef
Mauro Mandracchia <info@ideabile.com>
2014-03-15 02:33:49 +0100

    Add Main and Router

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+		var c = {
+			baseUrl: "",
+			// urlArgs: "bust=" +  (new Date()).getTime(),
+			paths: {
+				"underscore": "bower_components/underscore/underscore",
+				"backbone": "bower_components/backbone/backbone",
+				"paper": "bower_components/paper/dist/paper",
+				"paper-full": "bower_components/paper/dist/paper-full",
+				"jquery": "bower_components/jquery/dist/jquery.min",
+				"d3": "bower_components/d3/d3.min",
+				"magnific-popup": "bower_components/magnific-popup/dist/jquery.magnific-popup.min"
+			},
+			shim: {
+				'backbone': {
+					deps: ['underscore', 'jquery'],
+					exports: 'Backbone'
+				},
+				'magnific-popup': {
+					deps: ['jquery']
+				}
+			}
+		};
+
+		require.config(c);
+		require(["js/app"], function(app){
+			app.init();
+		});
